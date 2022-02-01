806000bf85245dc5c6edfc83105e4cea165fae76
badi3a <badiaabouhdid@gmail.com>
2022-02-01 16:37:34 +0100

    please find the code for of our second session

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/home")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/index", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+    /**
+     * @Route("/hello", name="helloPage")
+     */
+    public function hello(): Response
+    {
+        return $this->render('home/hello.html.twig');
+    }
+    /**
+     * @Route("/helloName", name="helloByNamePage")
+     */
+    public function helloByName(): Response
+    {   $name="Ali";
+        return $this->render('home/hello.html.twig',
+        [
+            "name"=>$name
+        ]);
+    }
+
+}
