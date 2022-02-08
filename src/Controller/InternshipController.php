0d415243cc787cdabab79f116d4b661e29c6eb8b
badi3a <badiaabouhdid@gmail.com>
2022-02-08 15:45:19 +0100

    SESSION 3.1: list internship

---
+++ src/Controller/InternshipController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InternshipController extends AbstractController
+{
+    /**
+     * @Route("/internship", name="internshipPage")
+     */
+    public function listInternship(): Response
+    {   $listInternship=[
+        ["ref"=>12, "title"=>"Angular Project", "company"=>"Vermeg","available"=>4],
+        ["ref"=>54, "title"=>"Symofny Project", "company"=>"Focus","available"=>0]
+    ];
+        return $this->render('internship/list.html.twig',
+        array('listInternship'=>$listInternship)
+        );
+    }
+
+
+}
