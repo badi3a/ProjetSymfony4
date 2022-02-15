ab73cb4e6b4dcb33feb1de7539de14f63e853d98
badi3a <badiaabouhdid@gmail.com>
2022-02-15 14:47:11 +0100

    Session 4.1

---
+++ src/Controller/ClassroomController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClassroomController extends AbstractController
+{
+    /**
+     * @Route("/classroom", name="classroom")
+     */
+    public function index(): Response
+    {
+        return $this->render('classroom/index.html.twig', [
+            'controller_name' => 'ClassroomController',
+        ]);
+    }
+}
